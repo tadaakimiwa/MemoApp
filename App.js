c053a9aca7145c008c0ea593482f2c90352dbfaf
三輪忠暉 <miwatadaaki@miwanotadashiakiranoMacBook-Pro.local>
2020-04-28 17:40:55 +0900

    Hyde Firebase certificate

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
+import firebase from 'firebase';
 
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
@@ -7,16 +8,31 @@ import MemoEditScreen from './src/screens/MemoEditScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
 
+import ENV from './env.json'
+
+const firebaseConfig = {
+  apiKey:            ENV.FIREBASE_API_KEY,
+  authDomain:        ENV.FIREBASE_AUTH_DOMAIN,
+  databaseURL:       ENV.FIREBASE_DB_URL,
+  projectId:         ENV.FIREBASE_PROJECT_ID,
+  storageBucket:     ENV.FIREBASE_STORAGE,
+  messagingSenderId: ENV.FIREBASE_SENDER_ID,
+  appId:             ENV.FIREBASE_APP_ID,
+  measurementId:     ENV.FIREBASE_MEASUREMENT_ID,
+};
+firebase.initializeApp(firebaseConfig);
 
 const App = createStackNavigator({
+  Login:      { screen: LoginScreen },
+  Signup:     { screen: SignupScreen },
   Home:       { screen: MemoListScreen },
   MemoDetail: { screen: MemoDetailScreen },
   MemoEdit:   { screen: MemoEditScreen },
-  Login:      { screen: LoginScreen },
-  Signup:     { screen: SignupScreen },
 }, {
   defaultNavigationOptions: {
     headerTitle: 'MEMOT',
+    headerTintColor: '#fff',
+    headerBackTitle: null,
     headerStyle: {
       backgroundColor: '#265366',
     },
